35ceddb8d970902ab2c2c6188fcb1f0606ac64b4
xinx1n <xin9142@qq.com>
2017-08-16 17:16:52 +0800

    监听回车 keyup 事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -2,7 +2,12 @@ import React,{Component} from 'react'
 
 class TodoInput extends Component{
   render(){
-    return <input type="text" defaultValue={this.props.content}/>
+    return <input type="text" defaultValue={this.props.content} onKeyUp={this.submit}/>
+  }
+  submit(e){
+    if(e.key==='Enter'){
+      console.log('回车了')
+    }
   }
 }
 export default TodoInput
