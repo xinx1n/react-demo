5dd6bcbb99a981aecd5eea16adecf91463b86476
xinx1n <xin9142@qq.com>
2017-08-18 10:24:38 +0800

    将数据存储到 localStorage

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import 'normalize.css'
 import './reset.css'
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
+import * as localStore from './localStore'
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
       newTodo:'',
-      todoList:[]
+      todoList:localStore.load('todoList')||[]
     }
   }
   render() {
@@ -60,6 +61,7 @@ class App extends Component {
       state.newTodo = ''
       return state
     })
+    localStore.save('todoList', this.state.todoList)
   }
   changeTitle(event){
     this.setState({
@@ -69,10 +71,12 @@ class App extends Component {
   toggle(e,todo){
     todo.status = !todo.status
     this.setState(this.state)
+    localStore.save('todoList', this.state.todoList)
   }
   delete(e,todo){
     todo.deleted = true
     this.setState(this.state)
+    localStore.save('todoList', this.state.todoList)
   }
 }
 let id = 0
